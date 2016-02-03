4e68bcb24e0ec820718a1f16546587eea6ef3e07
Ignacio Calvo <ignacio.calvo@frontiersin.org>
2016-02-03 18:21:01 +0100

    First commit

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.IO;
+
+public class Solution
+{
+    internal static void Main1()
+    {
+        Problem.Solve(Console.In, Console.Out);
+    }
+
+    public static void Main()
+    {
+
+        const int m = 300;
+        const int n = 300;
+        const int r = 1000000000;
+        File.Delete("biginput.txt");
+        using (var writer = File.CreateText("biginput.txt"))
+        {
+            writer.WriteLine("{0} {1} {2}", m, n, r);
+
+            int element = 100000000 - m * n - 1;
+            for (int row = 0; row < m; row++)
+            {
+                writer.Write(element);
+                element++;
+                for (int column = 1; column < n; column++)
+                {
+                    writer.Write(" " + element);
+                    element++;
+                }
+
+                writer.WriteLine();
+            }
+        }
+
+        using (var reader = File.OpenText("biginput.txt"))
+        {
+            File.Delete("bigoutput.txt");
+            using (var writer = File.CreateText("bigoutput.txt"))
+            {
+                Problem.Solve(reader, writer);
+            }
+        }
+    }
+}
+
+public static class Problem
+{
+    public static void Solve(TextReader reader, TextWriter writer)
+    {
+        var rotationInput = MatrixRotationInput.FromReader(reader);
+        var matrix = rotationInput.Matrix;
+        int rotations = rotationInput.Rotations;
+        var rotatedMatrix = matrix.Rotate(rotations);
+        
+        rotatedMatrix.Write(writer);
+    }
+
+
+    private static void Write<T>(this Matrix<T> matrix, TextWriter output)
+    {
+        for (int r = 0; r < matrix.RowCount; r++)
+        {
+            T[] row = new T[matrix.ColumnCount];
+            for (int c = 0; c < matrix.ColumnCount; c++)
+            {
+                row[c] = matrix[r, c];
+            }
+
+            output.WriteLine(string.Join(" ", row));
+        }
+    }
+
+}
+
+public class MatrixRotationInput
+{
+    private MatrixRotationInput(int rotations, Matrix<string> matrix)
+    {
+        if (rotations < 0)
+        {
+            throw new ArgumentOutOfRangeException("rotations", "Cannot have negative rotations");
+        }
+
+        if (matrix == null)
+        {
+            throw new ArgumentNullException("matrix");
+        }
+
+        Rotations = rotations;
+        Matrix = matrix;
+    }
+
+    public static MatrixRotationInput FromReader(TextReader input)
+    {
+        var allInput = input.ReadToEnd();
+        var lines = allInput.Split(new[] {"\r", "\n"}, StringSplitOptions.RemoveEmptyEntries);
+
+
+
+        string[] tokens = lines[0].Split(new [] { " " }, StringSplitOptions.RemoveEmptyEntries);
+        var rowCount = Convert.ToInt32(tokens[0]);
+        var columnCount = Convert.ToInt32(tokens[1]);
+        var rotations = Convert.ToInt32(tokens[2]);
+        var values = new Matrix<string>(rowCount, columnCount);
+        for (int row = 0; row < rowCount; row++)
+        {
+            var line = lines[row + 1].Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            for (int column = 0; column < columnCount; column++)
+            {
+                values[row, column] = line[column];
+            }
+        }
+
+        return new MatrixRotationInput(rotations, values);
+    }
+
+    public int Rotations { get; private set; }
+
+    public Matrix<string> Matrix { get; private set; }
+}
+
+public class Matrix<T>
+{
+    private readonly T[,] _values;
+
+    public Matrix(int rowCount, int columnCount)
+    {
+        if (rowCount < 0)
+        {
+            throw new ArgumentOutOfRangeException("rowCount", "Cannot have negative row count");
+        }
+
+        if (columnCount < 0)
+        {
+            throw new ArgumentOutOfRangeException("columnCount", "Cannot have negative column count");
+        }
+
+        RowCount = rowCount;
+        ColumnCount = columnCount;
+        _values = new T[RowCount, ColumnCount];
+    }
+
+    public int RowCount { get; private set; }
+
+    public int ColumnCount { get; private set; }
+
+    public T this[int rowIndex, int columnIndex]
+    {
+        get { return _values[rowIndex, columnIndex]; }
+        set
+        {
+            _values[rowIndex, columnIndex] = value;
+        }
+    }
+
+    public Matrix<T> Rotate(int numberOfRotations)
+    {
+        if (Math.Min(RowCount, ColumnCount) % 2 != 0)
+        {
+            throw new InvalidOperationException("Cannot rotate if Min(RowCount, ColumnCount) % 2 != 0");
+        }
+
+        var result = new Matrix<T>(RowCount, ColumnCount);
+
+        if (numberOfRotations < 0)
+        {
+            throw new ArgumentOutOfRangeException("numberOfRotations", "Cannot have negative rotations");
+        }
+
+        for (int row = 0; row < RowCount; row++)
+        {
+            for (int column = 0; column < ColumnCount; column++)
+            {
+                result[row, column] = ValueAtRotatedPosition(row, column, numberOfRotations);
+            }
+        }
+
+        return result;
+    }
+
+    private T ValueAtRotatedPosition(int row, int column, int numberOfRotations)
+    {
+        var position = new PositionOnMatrix(row, column, RowCount, ColumnCount);
+        var rotatedPosition = position.Rotate(numberOfRotations);
+        return _values[rotatedPosition.Row, rotatedPosition.Column];
+    }
+}
+
+public class PositionOnMatrix
+{
+    public PositionOnMatrix(int row, int column, int rowCount, int columnCount)
+    {
+        Row = row;
+        Column = column;
+        RowCount = rowCount;
+        ColumnCount = columnCount;
+    }
+
+    public int Row { get; private set; }
+
+    public int Column { get; private set; }
+
+    private int RowCount { get; set; }
+
+    private int ColumnCount { get; set; }
+
+    public PositionOnMatrix Rotate(int numberOfRotations)
+    {
+
+
+        PositionOnMatrix rotatedPosition = this;
+
+        ActionExtensions.Repeat(() =>
+        {
+            rotatedPosition = rotatedPosition.RotateOnce();
+        },
+        numberOfRotations);
+
+        return rotatedPosition;
+    }
+
+    private PositionOnMatrix RotateOnce()
+    {
+        if (MustGoDownWhenRotated())
+        {
+            return new PositionOnMatrix(Row + 1, Column, RowCount, ColumnCount);
+        }
+
+        if (MustGoUpWhenRotated())
+        {
+            return new PositionOnMatrix(Row - 1, Column, RowCount, ColumnCount);
+        }
+
+        if (MustGoRightWhenRotated())
+        {
+            return new PositionOnMatrix(Row, Column + 1, RowCount, ColumnCount);
+        }
+
+        if (MustGoLeftWhenRotated())
+        {
+            return new PositionOnMatrix(Row, Column - 1, RowCount, ColumnCount);
+        }
+
+        throw new InvalidOperationException("Should not get here");
+    }
+
+    private bool MustGoDownWhenRotated()
+    {
+        return
+            Row >= ColumnCount - Column - 1 &&
+            Column >= ColumnCount / 2 &&
+            Row < RowCount - ColumnCount + Column;
+    }
+
+    private bool MustGoUpWhenRotated()
+    {
+        return
+            Row >= Column + 1 &&
+            Column < ColumnCount / 2 &&
+            Row < RowCount - Column;
+    }
+
+    private bool MustGoRightWhenRotated()
+    {
+        return
+            Column >= Row &&
+            Row < RowCount / 2 &&
+            Column < ColumnCount - Row - 1;
+    }
+
+    private bool MustGoLeftWhenRotated()
+    {
+        return
+            Column >= RowCount - Row &&
+            Row >= RowCount / 2 &&
+            Column < ColumnCount - RowCount + Row + 1;
+    }
+}
+
+internal static class ActionExtensions
+{
+    internal static void Repeat(this Action action, int times)
+    {
+        for (int rotation = 1; rotation <= times; rotation++)
+        {
+            action();
+        }
+    }
+}
---
+++ ConsoleApplication2Tests/MatrixTests.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace Tests
+{
+    [TestClass]
+    public class MatrixTests
+    {
+        [TestMethod]
+        public void Test00()
+        {
+            Test(
+@"4 4 1
+1   2  3  4
+5   6  7  8
+9  10 11 12
+13 14 15 16",
+@"2 3 4 8
+1 7 11 12
+5 6 10 16
+9 13 14 15");
+        }
+
+        [TestMethod]
+        public void Test01()
+        {
+            Test(
+@"4 4 2
+1 2 3 4
+5 6 7 8
+9 10 11 12
+13 14 15 16",
+@"3 4 8 12
+2 11 10 16
+1 7 6 15
+5 9 13 14");
+        }
+
+        [TestMethod]
+        public void Test02()
+        {
+            Test(
+@"5 4 7
+1 2 3 4
+7 8 9 10
+13 14 15 16
+19 20 21 22
+25 26 27 28",
+@"28 27 26 25
+22 9 15 19
+16 8 21 13
+10 14 20 7
+4 3 2 1");
+        }
+
+        [TestMethod]
+        public void Test03()
+        {
+            Test(
+@"2 2 3
+1 1
+1 1",
+@"1 1
+1 1");
+        }
+
+        [TestMethod]
+        public void Test04()
+        {
+            Test(
+@"4 8 3
+  1  2  3  4  5  6  7  8
+  9 10 11 12 13 14 15 16
+ 17 18 19 20 21 22 23 24
+ 25 26 27 28 29 30 31 32",
+@"4 5 6 7 8 16 24 32
+3 13 14 15 23 22 21 31
+2 12 11 10 18 19 20 30
+1 9 17 25 26 27 28 29");
+        }
+        private void Test(string input, string expectedOutput)
+        {
+            var reader = new StringReader(input);
+            var writer = new StringWriter();
+            Problem.Solve(reader, writer);
+            var actualOutput = writer.ToString().Trim();
+
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+    }
+}
